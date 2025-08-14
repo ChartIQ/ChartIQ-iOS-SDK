437946334044710ff63e2a7e9d25e5e1a81a0abf
Jacob Richards <jacob_richards@VA034F7XM.mhf.mhc>
2025-08-14 09:21:59 -0400

    Release 4.4.0

---
+++ runDocs.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+jazzy --theme themes/chartiq --author SPGlobal --author_url https://www.spglobal.com/market-intelligence/en/solutions/products/chartiq --title 'Mobile SDK Version 4.3.0'
